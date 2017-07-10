ea8098e7606acb0a38d571642e749afaea68569a
MASHA <mashanikit@gmail.com>
2017-07-10 16:07:37 -0400

    run ajax request to root button

--- ajax.js
+++ ajax.js
@@ -1,9 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+
+// DECLARE VARIABLES
+var button = document.getElementById('req_root');
+
+// REGISTER EVENT HANDLERS
+button.addEventListener( 'click', function() {
+  console.log('requested root');
+
   $.ajax({
     url: 'http://first-ajax-api.herokuapp.com/',
     method: 'GET',
     dataType: 'HTML',
+    });
   });
-
 });
