8377dd4c7dbc1c712e1819508a0f144f5079612f
MASHA <mashanikit@gmail.com>
2017-07-11 14:14:30 -0400

    fail callback abd append complete

--- ajax.js
+++ ajax.js
@@ -25,15 +25,20 @@ button.addEventListener( 'click', function() {
     var pingSection = document.getElementById('step3456');
 
     $.ajax({
-      url: 'http://first-ajax-api.herokuapp.com/ping',
+      url: 'http://first-ajax-api.herokuapp.com/pingf',
       method: 'GET',
       dataType: 'HTML',
     }).done(function (responseData){
       console.log( responseData );
       pingSection.append( responseData );
-
-    });
-  })
+    }).fail(function () {
+  //In this callback, everyone panic! Our request has failed!
+  //Quickly, apologize to the user and try to fix it!
+      var error = "So sorry, your request has failed!";
+      console.log(error);
+      pingSection.append(error);
+  });
+});
 
 
 });
