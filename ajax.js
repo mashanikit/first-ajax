c7c39f6f3863d41c42f0c75fba95b5645f0d9adc
MASHA <mashanikit@gmail.com>
2017-07-11 14:10:00 -0400

    done callback and append completed

--- ajax.js
+++ ajax.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // DECLARE VARIABLES
 var button = document.getElementById('req_root');
+var pingPong = document.getElementById('ping_pong');
 
 // REGISTER EVENT HANDLERS
 button.addEventListener( 'click', function() {
@@ -12,6 +13,27 @@ button.addEventListener( 'click', function() {
     url: 'http://first-ajax-api.herokuapp.com/',
     method: 'GET',
     dataType: 'HTML',
-    });
+    }).done(function (responseData) {
+  //Here in the callback, we have a variable called responseData
+  //that holds the content of the server's response,
+  //in this case, a simple string
+});
   });
+
+  pingPong.addEventListener ('click', function() {
+
+    var pingSection = document.getElementById('step3456');
+
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/ping',
+      method: 'GET',
+      dataType: 'HTML',
+    }).done(function (responseData){
+      console.log( responseData );
+      pingSection.append( responseData );
+
+    });
+  })
+
+
 });
